644f713154e8c6a6160599cebd2759eaa48b4c08
Mai65 <finnh.spam@outlook.de>
2019-01-06 12:16:01 +0100

    improved messageboxes catched exceptions

--- venv/Main.py
+++ venv/Main.py
@@ -77,7 +77,6 @@ class Mask(ttk.Frame):
         birthdateMonthLabel = ttk.Label(self, text="Geburtsmonat")
         birthdateYearLabel = ttk.Label(self, text="Geburtsjahr")
 
-
         VornameLabel.grid(column=0, row=1)
         NachnameLabel.grid(column=0, row=2)
         StraßeLabel.grid(column=0, row=3)
@@ -120,8 +119,7 @@ class Mask(ttk.Frame):
             self.NummerEntry.grid(column=1, row=11)
             NummerLabel.grid(column=0, row=11)
 
-
-    def collect(self, number=False, checkForCompleteDate = False ):
+    def collect(self, number=False, checkForCompleteDate=False):
         """
 
         :type number: boolean
@@ -191,11 +189,11 @@ class Mask(ttk.Frame):
                 self.dict['Nummer'] = None
 
         if checkForCompleteDate is True:
-            if self.birthdateYearEntry.get() and not self.birthdateMonthEntry.get() or not self.birthdateDayEntry.get():
+            if self.birthdateYearEntry.get() and (not self.birthdateMonthEntry.get() or not self.birthdateDayEntry.get()):
                 return 1
-            elif self.birthdateMonthEntry.get() and not self.birthdateYearEntry.get()  or not self.birthdateDayEntry.get() :
+            elif self.birthdateMonthEntry.get() and (not self.birthdateYearEntry.get() or not self.birthdateDayEntry.get()):
                 return 1
-            elif self.birthdateDayEntry.get() and not self.birthdateYearEntry.get()  or not self.birthdateMonthEntry.get() :
+            elif self.birthdateDayEntry.get() and (not self.birthdateYearEntry.get() or not self.birthdateMonthEntry.get()):
                 return 1
         if is_empty is True:
             return None
@@ -206,24 +204,24 @@ class Mask(ttk.Frame):
 class hinzufügen():
     def __init__(self, parent):
         mask = Mask(parent, False)
-        self.parent = parent
-        ttk.Button(mask, text='Ausführen', command=(lambda: insert(mask.collect(False)) if mask.collect(False,True) is not 1 and dict is not None else messagebox.showerror("Error", "Sie haben nur Teile des Geburtsdatums angegeben. Bitt vervollständigen")), width=10).grid(
+
+        ttk.Button(mask, text='Ausführen', command=(
+            lambda: (insert(mask.collect(False)) if mask.collect(False, True) is not None and mask.collect(False, True) is not 1 else (
+                messageboxes.incomplete_birthdate() if mask.collect(False, True) is 1 else messageboxes.no_entry_in_mask()),mask.destroy(), hinzufügen(parent))),
+                   width=10).grid(
             column=1, row=11)
         ttk.Button(mask, text="zurück", command=mask.destroy, width=10).grid(
             column=0, row=11)
 
 
-
-
 class abfragen():
     def __init__(self, parent):
-        self.mask = Mask(parent, True)
-        self.parent = parent
-        self.ausführen = ttk.Button(self.mask, text='Ausführen',
-                                    command=lambda: display_entry(self.parent, self.search(self.mask.collect(True))),
+        mask = Mask(parent, True)
+        parent = parent
+        ausführen = ttk.Button(mask, text='Ausführen', command=lambda: display_entry(parent, self.search(mask.collect(True))) if mask.collect(True, False) is not None and self.search(mask.collect(True)) else (messageboxes.no_entry_in_mask() if mask.collect(True, False) is None else messageboxes.no_entry_in_DB()),
                                     width=10)
-        self.ausführen.grid(column=1, row=12)
-        ttk.Button(self.mask, text="zurück", command= self.mask.destroy, width=10).grid(
+        ausführen.grid(column=1, row=12)
+        ttk.Button(mask, text="zurück", command=mask.destroy, width=10).grid(
             column=0, row=12)
 
     def search(self, dict):
@@ -232,114 +230,9 @@ class abfragen():
                                       database='adressbuch')
 
         cursor = cnx.cursor()
-        if dict is None:
-            messagebox.showerror("Error", "Sie haben keine Werte eingegeben. Bitte versuchen sie es erneut")
-            self.parent.destroy()
-            main = Main()
+        sql_and_data = make_get_and_data_sql(dict)
 
-        vorgaenger = False
-        get = "SELECT * FROM `adressbuch` WHERE "
-
-        if dict['Vorname'] is not None and vorgaenger is False:
-            get += " Vorname = %s"
-            data = (dict['Vorname'],)
-            vorgaenger = True
-        if dict['Nachname'] is not None and vorgaenger is False:
-            get += " Nachname = %s"
-            data = (dict['Nachname'],)
-            vorgaenger = True
-        elif dict['Nachname'] is not None and vorgaenger is not False:
-            get += " and Nachname = %s"
-            li = list(data)
-            li.append(dict['Nachname'])
-            data = tuple(li)
-        if dict['Straße'] is not None and vorgaenger is False:
-            get += " Straße = %s"
-            data = (dict['Straße'],)
-            vorgaenger = True
-        elif dict['Straße'] is not None and vorgaenger is not False:
-            get += " and Straße = %s"
-            li = list(data)
-            li.append(dict['Straße'])
-            data = tuple(li)
-        if dict['HausNr'] is not None and vorgaenger is False:
-            get += " HausNr = %s"
-            data = (dict['HausNr'],)
-            vorgaenger = True
-        elif dict['HausNr'] is not None and vorgaenger is not False:
-            get += " and HausNr = %s"
-            li = list(data)
-            li.append(dict['HausNr'])
-            data = tuple(li)
-        if dict['Ort'] is not None and vorgaenger is False:
-            get += " Ort = %s"
-            data = (dict['Ort'],)
-            vorgaenger = True
-        elif dict['Ort'] is not None and vorgaenger is not False:
-            get += " and Ort = %s"
-            li = list(data)
-            li.append(dict['Ort'])
-            data = tuple(li)
-        if dict['PLZ'] is not None and vorgaenger is False:
-            get += " PLZ = %s"
-            data = (dict['PLZ'],)
-            vorgaenger = True
-        elif dict['PLZ'] is not None and vorgaenger is not False:
-            get += " and PLZ = %s"
-            li = list(data)
-            li.append(dict['PLZ'])
-            data = tuple(li)
-        if dict['Land'] is not None and vorgaenger is False:
-            get += " Land = %s"
-            data = (dict['Land'],)
-            vorgaenger = True
-        elif dict['Land'] is not None and vorgaenger is not False:
-            get += " and Land = %s"
-            li = list(data)
-            li.append(dict['Land'])
-            data = tuple(li)
-
-
-        if dict['birthdateDay'] is not None and vorgaenger is False:
-            get += " DAY(birthdate)= %s"
-            data = (dict['birthdateDay'],)
-            vorgaenger = True
-        elif dict['birthdateDay'] is not None and vorgaenger is not False:
-            get += " and DAY(birthdate) = %s"
-            li = list(data)
-            li.append(dict['birthdateDay'])
-            data = tuple(li)
-        if dict['birthdateMonth'] is not None and vorgaenger is False:
-            get += " MONTH(birthdate)= %s"
-            data = (dict['birthdateMonth'],)
-            vorgaenger = True
-        elif dict['birthdateMonth'] is not None and vorgaenger is not False:
-            get += " and MONTH(birthdate) = %s"
-            li = list(data)
-            li.append(dict['birthdateMonth'])
-            data = tuple(li)
-        if dict['birthdateYear'] is not None and vorgaenger is False:
-            get += " YEAR(birthdate)= %s"
-            data = (dict['birthdateYear'],)
-            vorgaenger = True
-        elif dict['birthdateYear'] is not None and vorgaenger is not False:
-            get += " and YEAR(birthdate) = %s"
-            li = list(data)
-            li.append(dict['birthdateYear'])
-            data = tuple(li)
-
-
-        if dict['Nummer'] is not None and vorgaenger is False:
-            get += " Nummer = %s"
-            data = (dict['Nummer'],)
-            vorgaenger = True
-        elif dict['Nummer'] is not None and vorgaenger is not False:
-            get += " and Nummer = %s"
-            li = list(data)
-            li.append(dict['Nummer'])
-            data = tuple(li)
-
-        cursor.execute(get, data)
+        cursor.execute(list(sql_and_data)[0], list(sql_and_data)[1])
         result = cursor.fetchall()
         result_list = []
         for x in result:
@@ -349,15 +242,12 @@ class abfragen():
         return result_list
 
 
-
 class display_entry(ttk.Frame):
     def __init__(self, parent, list):
         super().__init__(parent)
         self.grid(column=0, row=0, sticky=(W + N + S + E))
 
-
         list = np.array(list).reshape(int(len(list) / 9), 9).tolist()
-
 
         display_entry_frames = []
         for x in range(len(list)):
@@ -375,20 +265,19 @@ class display_entry(ttk.Frame):
         display_entry_frames[self.current_page].grid(column=0, row=1)
         ttk.Button(self, text="Zurück", command=self.destroy, width=10).grid(
             column=0, row=3)
-        ttk.Button(self, text="Update", command=lambda: (parent.destroy(), Main(), sys.exit) if updat_entry(display_entry_frames[self.current_page].mask, True) is None  else None, width=10).grid(
+        ttk.Button(self, text="Update", command=lambda: (parent.destroy(), Main(), sys.exit) if updat_entry(
+            display_entry_frames[self.current_page].mask, True) is None else None, width=10).grid(
             column=1, row=3)
 
         def update_page(minus):
             if len(list) is not 1:
                 display_entry_frames[self.current_page].grid_forget()
                 if minus:
-                    self.current_page = (self.current_page - 1) % (len(list) )
+                    self.current_page = (self.current_page - 1) % (len(list))
                 else:
-                    self.current_page = (self.current_page + 1) % (len(list) )
+                    self.current_page = (self.current_page + 1) % (len(list))
 
                 display_entry_frames[self.current_page].grid(column=0, row=1)
-
-
 
 
 class display_entry_frame(ttk.Frame):
@@ -414,22 +303,35 @@ class display_entry_frame(ttk.Frame):
         mask.birthdateMonthEntry.insert(0, temp[1])
         mask.birthdateYearEntry.insert(0, temp[0])
         mask.NummerEntry.grid_forget()
-        NummerLabelDisplay = ttk.Label(mask, text = str(list[8]))
+        NummerLabelDisplay = ttk.Label(mask, text=str(list[8]))
         NummerLabelDisplay.grid(column=1, row=11)
         mask.NummerEntry.insert(0, str(list[8]))
 
-    def fillup_None(self,list):
+    def fillup_None(self, list):
         for x in range(len(list)):
             if list[x] is None:
                 list[x] = "-"
 
 
-def insert( dict, withNumber=False):
-    #if dict is None:
-    #    messagebox.showerror("Error", "Sie haben keine Werte eingegeben. Bitte versuchen sie es erneut")
-    #    self.parent.destroy()
-     #   Main()
-     #   sys.exit()
+class messageboxes():
+
+    @staticmethod
+    def incomplete_birthdate():
+        messagebox.showerror("Error", "Sie haben nur Teile des Geburtsdatums angegeben. Bitte vervollständigen sie es.")
+
+    @staticmethod
+    def no_entry_in_mask():
+        messagebox.showerror("Error", "Sie haben keinen Wert eingegeben. Bitte versuchen sie es erneut.")
+
+    @staticmethod
+    def no_entry_in_DB():
+        messagebox.showerror("Error", "Ihr angeforderter Eintrag existiert nicht")
+
+    @staticmethod
+    def entry_updated():
+        messagebox.showinfo("Updated", "Eintrag erfolgreich aktuallisiert")
+
+def insert(dict, withNumber=False):
 
     cnx = mysql.connector.connect(user='python', password='',
                                   host='127.0.0.1',
@@ -490,7 +392,8 @@ def make_date(dict):
 
     return date
 
-def updat_entry(mask, withNumber = False):
+
+def updat_entry(mask, withNumber=False):
     cnx = mysql.connector.connect(user='python', password='',
                                   host='127.0.0.1',
                                   database='adressbuch')
@@ -502,13 +405,115 @@ def updat_entry(mask, withNumber = False):
         if dict[x] is '-':
             dict[x] = None
 
-
     delete = "DELETE FROM `adressbuch` WHERE (Nummer = %s)"
-    data = (dict['Nummer'], )
+    data = (dict['Nummer'],)
 
     cursor.execute(delete, data)
     cnx.commit()
     cnx.close()
     insert(dict, True)
-    messagebox.showerror("Updated", "Eintrag erfolgreich aktuallisiert")
+    messageboxes.entry_updated()
+
+def make_get_and_data_sql(dict):
+    vorgaenger = False
+    get = "SELECT * FROM `adressbuch` "
+
+    if dict['Vorname'] is not None and vorgaenger is False:
+        get += " WHERE Vorname = %s"
+        data = (dict['Vorname'],)
+        vorgaenger = True
+    if dict['Nachname'] is not None and vorgaenger is False:
+        get += " WHERE Nachname = %s"
+        data = (dict['Nachname'],)
+        vorgaenger = True
+    elif dict['Nachname'] is not None and vorgaenger is not False:
+        get += " and Nachname = %s"
+        li = list(data)
+        li.append(dict['Nachname'])
+        data = tuple(li)
+    if dict['Straße'] is not None and vorgaenger is False:
+        get += " WHERE Straße = %s"
+        data = (dict['Straße'],)
+        vorgaenger = True
+    elif dict['Straße'] is not None and vorgaenger is not False:
+        get += " and Straße = %s"
+        li = list(data)
+        li.append(dict['Straße'])
+        data = tuple(li)
+    if dict['HausNr'] is not None and vorgaenger is False:
+        get += " WHERE HausNr = %s"
+        data = (dict['HausNr'],)
+        vorgaenger = True
+    elif dict['HausNr'] is not None and vorgaenger is not False:
+        get += " and HausNr = %s"
+        li = list(data)
+        li.append(dict['HausNr'])
+        data = tuple(li)
+    if dict['Ort'] is not None and vorgaenger is False:
+        get += " WHERE Ort = %s"
+        data = (dict['Ort'],)
+        vorgaenger = True
+    elif dict['Ort'] is not None and vorgaenger is not False:
+        get += " and Ort = %s"
+        li = list(data)
+        li.append(dict['Ort'])
+        data = tuple(li)
+    if dict['PLZ'] is not None and vorgaenger is False:
+        get += " WHERE PLZ = %s"
+        data = (dict['PLZ'],)
+        vorgaenger = True
+    elif dict['PLZ'] is not None and vorgaenger is not False:
+        get += " and PLZ = %s"
+        li = list(data)
+        li.append(dict['PLZ'])
+        data = tuple(li)
+    if dict['Land'] is not None and vorgaenger is False:
+        get += " WHERE Land = %s"
+        data = (dict['Land'],)
+        vorgaenger = True
+    elif dict['Land'] is not None and vorgaenger is not False:
+        get += " and Land = %s"
+        li = list(data)
+        li.append(dict['Land'])
+        data = tuple(li)
+
+    if dict['birthdateDay'] is not None and vorgaenger is False:
+        get += " WHERE DAY(birthdate)= %s"
+        data = (dict['birthdateDay'],)
+        vorgaenger = True
+    elif dict['birthdateDay'] is not None and vorgaenger is not False:
+        get += " and DAY(birthdate) = %s"
+        li = list(data)
+        li.append(dict['birthdateDay'])
+        data = tuple(li)
+    if dict['birthdateMonth'] is not None and vorgaenger is False:
+        get += " WHERE MONTH(birthdate)= %s"
+        data = (dict['birthdateMonth'],)
+        vorgaenger = True
+    elif dict['birthdateMonth'] is not None and vorgaenger is not False:
+        get += " and MONTH(birthdate) = %s"
+        li = list(data)
+        li.append(dict['birthdateMonth'])
+        data = tuple(li)
+    if dict['birthdateYear'] is not None and vorgaenger is False:
+        get += " WHERE YEAR(birthdate)= %s"
+        data = (dict['birthdateYear'],)
+        vorgaenger = True
+    elif dict['birthdateYear'] is not None and vorgaenger is not False:
+        get += " and YEAR(birthdate) = %s"
+        li = list(data)
+        li.append(dict['birthdateYear'])
+        data = tuple(li)
+
+    if dict['Nummer'] is not None and vorgaenger is False:
+        get += " WHERE Nummer = %s"
+        data = (dict['Nummer'],)
+    elif dict['Nummer'] is not None and vorgaenger is not False:
+        get += " and Nummer = %s"
+        li = list(data)
+        li.append(dict['Nummer'])
+        data = tuple(li)
+
+    return get, data
+
 Main()
